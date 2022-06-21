93d97ba10475a4a5404ad901551bf6ac3044cd52
Nathan Perry <np@nathanperry.dev>
2022-06-21 00:12:34 -0400

    fix shutdown failure

--- src/standard_graph.rs
+++ src/standard_graph.rs
@@ -153,15 +153,82 @@ pub async fn run<Socket>(
         CRCMessage::new(Header::downlink(packet_env, Conversation::Relay), uplink_pkt)
     });
 
-    let downlink_packets = futures::stream_select![
-        wrapped_uplink,
-        trace_event.pipe(trip!(noclone tripwire)),
-        wrapped_serial_downlink
-    ]
-    .map(&*ser)
-    .pipe(stream_unwrap!("encoding downlink message"));
+    let downlink_packets =
+        futures::stream_select![wrapped_uplink, trace_event, wrapped_serial_downlink]
+            .map(&*ser)
+            .pipe(trip!(tripwire))
+            .pipe(stream_unwrap!("encoding downlink message"));
 
     let downlink = relay::send_downlink::<Socket>(downlink_packets, downlink_sockets);
 
     futures::future::join3(pump_uplink_to_serial, uplink_pump, downlink).await;
+}
+
+#[cfg(test)]
+mod test {
+    use smol::channel::Sender;
+
+    use crate::util::{
+        timeout,
+        DEFAULT_LINK_CODEC,
+        DEFAULT_SERIAL_CODEC,
+    };
+
+    use super::*;
+
+    #[async_std::test]
+    async fn serial_shutdown() {
+        let (tx_done, rx_done) = smol::channel::bounded(1);
+
+        let (pump, _cs, s) = serial(
+            (DEFAULT_SERIAL_CODEC.clone(), PacketEnv::default()),
+            smol::io::sink(),
+            smol::io::repeat(0u8),
+            rx_done,
+        );
+
+        let task = smol::spawn(async move {
+            smol::future::zip(s.for_each(|_| {}), pump).await;
+        });
+
+        tx_done.close();
+
+        timeout(500, task).await.unwrap();
+    }
+
+    #[async_std::test]
+    async fn run_shutdown() {
+        let (tx_done, rx_done) = smol::channel::bounded(1);
+
+        let (csq, s1, s2) = CommandSequencer::new(smol::stream::empty());
+        let (tx, rx) = smol::channel::unbounded();
+
+        smol::spawn(async move {
+            let f1 = s1.for_each(|_| {});
+            let f2 = s2.for_each(|_| {});
+            let f3 = rx.for_each(|_| {});
+
+            futures::future::join3(f1, f2, f3).await;
+        })
+        .detach();
+
+        let task = smol::spawn(run::<Sender<Vec<u8>>>(
+            (DEFAULT_LINK_CODEC.clone(), PacketEnv::default()),
+            smol::stream::repeat(vec![]),
+            std::iter::once(smol::stream::repeat(tx)),
+            smol::stream::empty(),
+            Arc::new(csq),
+            smol::stream::repeat_with(|| {
+                CRCMessage::new(
+                    Header::cs_command(&PacketEnv::default(), Conversation::Calibrate),
+                    vec![],
+                )
+            }),
+            rx_done,
+        ));
+
+        tx_done.close();
+
+        timeout(500, task).await.unwrap();
+    }
 }
